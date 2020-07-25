fde00fa9a668378145a43beffc3f5a934a3203e8
Dora He <kamiyajelly@gmail.com>
2020-07-25 01:26:04 -0400

    style the footer component and detail compoent, delete a few unused dependencies in package.json

--- src/components/AdoptionDetail/AdoptionDetail.js
+++ src/components/AdoptionDetail/AdoptionDetail.js
@@ -16,6 +16,8 @@ export default function AdoptionDetail() {
           
         </section>
         <section className="adoptionDetail__hero-right">
+            <div className="adoptionDetail__card adoptionDetail__card-greeting">
+                
             <h5 className="adoptionDetail__greeting">
                 Hi! I'm
             </h5>
@@ -27,37 +29,43 @@ export default function AdoptionDetail() {
             <div>Domestic Short Hair - Toronto, ON</div>
             </section>
             <section className="adoptionDetail__tags">
-                <TagGroup>
+                <TagGroup style={{marginRight:'1rem'}}>
                     <Tag style={{backgroundColor:'#D775A3', color:'white'}}>Cute</Tag>
-                    <Tag>Black</Tag>
+                    <Tag style={{backgroundColor:'#D775A3', color:'white'}}>Black</Tag>
+                    <Tag style={{backgroundColor:'#D775A3', color:'white'}}>Black</Tag>
+                    <Tag style={{backgroundColor:'#D775A3', color:'white'}}>Black</Tag>
+
                 </TagGroup>
             </section>
+            </div>
         </section>
       </section>
 
 <section className="adoptionDetail__body">
-    <section className="adoptionDetail__shelter">
-        <h1 className="adoptionDetail__shelterHeading">Shelter / Rescue Group</h1>
-        <h5 className="adoptionDetail__shelterName">
+    <section className="adoptionDetail__shelter adoptionDetail__card">
+        <h1 className="adoptionDetail__shelter-heading">Shelter / Rescue Group</h1>
+        <h5 className="adoptionDetail__shelter-name">
             Safehome animal rescue
         </h5>
 
         <section className="adoptionDetail__shelter-contact">
-            <img src="" alt="" className="adoptionDetail__icon"/>
+            <img src="./assets/svg/location.svg" alt="" className="adoptionDetail__shelter-icon"/>
             <div className="adoptionDetail__location">Toronto, ON</div>
         </section>
         <section className="adoptionDetail__shelter-contact">
-            <img src="" alt="" className="adoptionDetail__icon"/>
-            <div className="adoptionDetail__email">Toronto, ON</div>
+            <img src="./assets/svg/Icon-email.svg" alt="" className="adoptionDetail__shelter-icon"/>
+            <div className="adoptionDetail__email">rescue@gmail.com</div>
         </section>
         <section className="adoptionDetail__shelter-contact">
-            <img src="" alt="" className="adoptionDetail__icon"/>
-            <div className="adoptionDetail__phone">Toronto, ON</div>
+            <img src="./assets/svg/Icon-phone.svg" alt="" className="adoptionDetail__shelter-icon"/>
+            <div className="adoptionDetail__phone">123-456-789</div>
         </section>
     </section>
-    <section className="adoptionDetail__story">
+    <section className="adoptionDetail__story adoptionDetail__card">
         <h1 className='adoptionDetail__story-heading'>My Story</h1>
-        <div className="adoptionDetail__story-body">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi aliquid, maxime quibusdam asperiores facilis id delectus suscipit dignissimos explicabo sequi excepturi deleniti dolores odit sed labore saepe fugit? Numquam, explicabo.</div>
+        <div className="adoptionDetail__story-body">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi aliquid, maxime quibusdam asperiores facilis id delectus suscipit dignissimos explicabo sequi excepturi deleniti dolores odit sed labore saepe fugit? Numquam, explicabo. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio nesciunt culpa dolorem reiciendis inventore cumque repellendus eaque, deleniti, aut recusandae obcaecati itaque impedit fugiat necessitatibus fuga exercitationem, dolores modi nisi?
+        Corporis iure eligendi sequi necessitatibus. Cum quo, reiciendis odio quam accusamus optio fugiat commodi. Totam, voluptatibus? Dignissimos libero illo, iusto architecto maiores facere voluptates cumque beatae ducimus velit atque fugiat!
+        Nemo facere quidem consectetur recusandae? Voluptates saepe voluptas cum sapiente. Iste assumenda odit officia numquam et, consectetur officiis. Neque distinctio nihil et iste quisquam error facere hic eligendi. Iste, necessitatibus.</div>
     </section>
 </section>
     </section>
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './style.scss'
 
 export default function Footer() {
     return (
@@ -7,39 +8,47 @@ export default function Footer() {
                 <div className="footer__card footer__card-one">
                     <div className="footer__card-heading">
                     <img src="./assets/svg/Icon-phone.svg" alt="phone icon"/>
-                    <div className="footer__card-heading-txt">1-647-978-0544</div>
+                    <h3 className="footer__card-heading-txt">1-647-978-0544</h3>
                     </div>
-                    <div className="footer__card-body">
-                    Mon ~ Fri  9:00 ~ 18:00
+                    <div className="footer__card-body footer__card-workTime">
+                    <div className="footer__card-week">
+                    Mon ~ Fri
+                    </div> 
+                    <div className="footer__card-time">
+                    9:00 ~ 18:00
+                    </div>
                     </div>
                 </div>
                 <div className="footer__card footer__card-two">
                     <div className="footer__card-heading">
                     <img src="./assets/svg/Icon-email.svg" alt="mail icon"/>
-                    <div className="footer__card-heading-txt">1-647-978-0544</div>
+                    <h2 className="footer__card-heading-txt">CONTACT</h2>
                     </div>
                     <div className="footer__card-body">
-                    Mon ~ Fri  9:00 ~ 18:00
+                    catrescue@cathome.com
                     </div>
                 </div>
             </div>
 
             <div className="footer__icons">
-            <div className="footer__logo">
-                <img src="./assets/svg/logo.svg" alt="blue logo of cat home"/>
-            </div>
+
+                <img src="./assets/logo.svg" alt="blue logo of cat home" className='footer__logo'/>
             <div className="footer__share">
                 <img src="./assets/svg/facebook.svg" alt="The share button for facebook" className="footer__facebook"/>
-                <img src="./assets/svg/twitter.svg" alt="The share button for twitter" className="footer__twitter"/>
+                <img src="./assets/svg/twitter.svg" alt="The share button for twitter" className="footer__instagram"/>
+                <img src="./assets/svg/instagram.svg" alt="The share button for instagram" className="footer__facebook"/>
+                <img src="./assets/svg/behance.svg" alt="The share button for behance" className="footer__behance"/>
             </div>
             </div>
-
+            {/* <div className="footer__nav-wrapper">
             <div className="footer__nav">
                 <div>HOME</div>
-                <div>SUPPORT</div>
+                <div className='footer__nav-end'>SUPPORT</div>
                 <div>ADOPTION</div>
-                <div>STORY</div>
+                <div className='footer__nav-end'>STORY</div>
             </div>
+
+            </div> */}
 
         </div>
     )
